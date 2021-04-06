5e005cc89e5c6a7c73bce06eaf05a17ad9dddc5f
India Marsden <imm1117@ic.ac.uk>
2021-04-06 16:08:24 +0100

    moved it server side

--- app.js
+++ app.js
@@ -11,23 +11,24 @@ app.use("/", express.static("static"));
 app.listen(port, function () {
     console.log("Listening on port " + port);
     console.log("http://localhost:" + port);
+    // res.send("Updating Breaker")
+    const {breakerCount, maxBreakerCount} = fs.readFile('/breaker.json', (err, content) => {
+        if (err) return res.send('Error loading breaker file:', err);
+        return JSON.parse(content);
+    });
+    // res.send(data);
+    // const {increment} = JSON.parse(req.body);
+    // // res.send(increment)
+    breakerCount += increment;
+    maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
+    fs.writeFile('/breaker.json', {'breakerCount': breakerCount, 'maxBreakerCount': maxBreakerCount});
+    console.log(breakerCount + " : " + maxBreakerCount);
 });
 
 app.use("/cal/", function (req, res) {
     calender.getCalEvents(res);
 });
 
-app.use("/breaker_update/", function (req, res) {
-    console.log("Updating Breaker");
-    // res.send("Updating Breaker")
-    const data = fs.readFile('/breaker.json', (err, content) => {
-        if (err) return res.send('Error loading breaker file:', err);
-        return JSON.parse(content);
-    });
-    res.send(data);
-    // const {increment} = JSON.parse(req.body);
-    // // res.send(increment)
-    // breakerCount += increment;
-    // maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
-    // fs.writeFile('/breaker.json', {'breakerCount': breakerCount, 'maxBreakerCount': maxBreakerCount});
-});
+// app.use("/breaker_update/", function (req, res) {
+//     console.log("Updating Breaker");
+// });
