78d26f01e1ed28ee648b86389b7ea06437ea1987
India Marsden <imm1117@ic.ac.uk>
2021-04-06 16:25:17 +0100

    we're very bad at this

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.listen(port, function () {
     // res.send("Updating Breaker")
     fs.readFile('breaker.json', (err, content) => {
         if (err) return console.log('Error loading breaker file:', err);
-        data = JSON.parse(content);
+        let {breakerCount, maxBreakerCount} = JSON.parse(content);
+        breakerCount += 1;
+        maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
+        fs.writeFile('breaker.json', JSON.stringify({breakerCount: breakerCount, maxBreakerCount: maxBreakerCount}));
+        console.log(breakerCount + " : " + maxBreakerCount);
     });
     // res.send(data);
     // const {increment} = JSON.parse(req.body);
     // // res.send(increment)
-    breakerCount += 1;
-    maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
-    fs.writeFile('breaker.json', JSON.stringify({breakerCount: breakerCount, maxBreakerCount: maxBreakerCount}));
-    console.log(breakerCount + " : " + maxBreakerCount);
 });
 
 app.use("/cal/", function (req, res) {
