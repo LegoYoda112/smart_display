cfa9201f3431208c934f96bcf1703faabec4a9ad
India Marsden <imm1117@ic.ac.uk>
2021-04-06 16:33:11 +0100

    I'm so confused rn

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 const port = 8080;
 const app = express();
 
+const breaker = require('./breaker');
+
 app.use("/", express.static("static"));
 
 // Start server
@@ -12,14 +14,14 @@ app.listen(port, function () {
     console.log("Listening on port " + port);
     console.log("http://localhost:" + port);
     // res.send("Updating Breaker")
-    fs.readFile('breaker.json', 'utf-8', (err, content) => {
-        if (err) return console.log('Error loading breaker file:', err);
-        let {breakerCount, maxBreakerCount} = JSON.parse(content);
-        // breakerCount += 1;
-        // maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
-        fs.writeFile('breaker.json', JSON.stringify({breakerCount: breakerCount, maxBreakerCount: maxBreakerCount}));
-        console.log(breakerCount + " : " + maxBreakerCount);
-    });
+    // fs.readFile('breaker.json', 'utf-8', (err, content) => {
+    //     if (err) return console.log('Error loading breaker file:', err);
+    //     let {breakerCount, maxBreakerCount} = JSON.parse(content);
+    //     // breakerCount += 1;
+    //     // maxBreakerCount = Math.max(breakerCount, maxBreakerCount);
+    //     fs.writeFile('breaker.json', JSON.stringify({breakerCount: breakerCount, maxBreakerCount: maxBreakerCount}));
+    //     console.log(breakerCount + " : " + maxBreakerCount);
+    // });
     // res.send(data);
     // const {increment} = JSON.parse(req.body);
     // // res.send(increment)
@@ -29,6 +31,6 @@ app.use("/cal/", function (req, res) {
     calender.getCalEvents(res);
 });
 
-// app.use("/breaker_update/", function (req, res) {
-//     console.log("Updating Breaker");
-// });
+app.get("/breaker", function (req, res) {
+    res.send(breaker);
+});
